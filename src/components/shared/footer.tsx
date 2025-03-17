29a604eb3d10bd24df7608b710889ed21fda6e75
OnaDebrah <o.debrah@stuart.com>
2025-03-17 22:01:21 +0100

    Added headers, footers and menu

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {ChevronUp} from "lucid-react"
+import {ChevronUp} from "lucide-react"
 import {Button} from "../ui/button"
 import Link from "next/link"
 import {APP_NAME} from "@/lib/constants"
