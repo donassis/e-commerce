78030cf74697e6fa4db7386add539c1b3647deda
OnaDebrah <o.debrah@stuart.com>
2025-03-17 21:29:22 +0100

    Add GitHub Actions CI/CD

---
+++ src/components/shared/footer.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import {ChevronUp} from "lucid-react"
+import {Button} from "../ui/button"
+import Link from "next/link"
+import {APP_NAME} from "@/lib/constants"
+
+export default function Footer() {
+    return (
+        <footer className="bg-black text-white underline-link">
+            <div className="w-full">
+            <Button
+            variant="ghost"
+            className="bg-grey-800 w-full rounded-none"
+            onClick={() => window.scrollTo({top: 0, behavior: "smooth"})}
+            >
+                <ChevronUp className="mr-2 h-4 w-4" />
+                Back to top
+            </Button>
+            </div>
+            <div className="p-4">
+            <div className="flex justify-center gap-3 text-sm">
+                <Link href="/page/conditions-of-use">Conditions of Use</Link>
+                <Link href="/page/privacy-notice">Privacy Notice</Link>
+                <Link href="/page/help">Help</Link>
+            </div>
+            <div className="flex justify-center text-sm">
+                © {new Date().getFullYear()} {APP_NAME}
+            </div>
+            <div className="mt-8 flex justify-center text-sm text-gray-400">
+            1234 Main St, Anytown, USA
+            </div>
+            </div>
+        </footer>
+    )
+}
+
+
+
+
+
+
+
+
