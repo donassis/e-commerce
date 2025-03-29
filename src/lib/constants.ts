7d4ce2133f2c7ccb872403759fa2c73278b64acd
OnaDebrah <o.debrah@stuart.com>
2025-03-29 08:25:41 +0100

    improved image display

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'BestFind'
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_SLOGAN || 'Spend Less, Enjoy More'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Spend Less, Enjoy More'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
