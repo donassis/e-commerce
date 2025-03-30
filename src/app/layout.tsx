8e02c193d05942209dcedf936671d97562876c62
OnaDebrah <o.debrah@stuart.com>
2025-03-30 16:45:28 +0200

    added signin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,40 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {APP_DESCRIPTION, APP_NAME, APP_SLOGAN} from "@/lib/constants"
 import ClientProviders from "@/components/shared/client-providers";
+import React from "react";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: {
-      template: `%s | ${APP_SLOGAN}`,
-      default: `${APP_NAME}. ${APP_SLOGAN}`,
-      },
-  description: APP_DESCRIPTION,
+    title: {
+        template: `%s | ${APP_SLOGAN}`,
+        default: `${APP_NAME}. ${APP_SLOGAN}`,
+    },
+    description: APP_DESCRIPTION,
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-       <ClientProviders> {children} </ClientProviders>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <ClientProviders> {children} </ClientProviders>
+        </body>
+        </html>
+    );
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,8 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'BestFind'
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_SLOGAN || 'Spend Less, Enjoy More'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Spend Less, Enjoy More'
+export const APP_COPYRIGHT = process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+`Copyright © 2025 ${APP_NAME}. All rights reserved.`
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 export const FREE_SHIPPING_MIN_PRICE = Number(process.env.FREE_SHIPPING_MIN_PRICE || 35)
